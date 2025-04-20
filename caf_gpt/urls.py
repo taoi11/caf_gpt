8088e2c60f516e7b3ef900bad780ed2a7993ba80
Luffy <github@luffy.email>
2025-04-20 16:00:16 -0600

    ...

--- caf_gpt/urls.py
+++ caf_gpt/urls.py
@@ -1,18 +1,5 @@
 """
-URL configuration for caf_gpt project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+CAF_GPT URL Configuration
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -29,7 +16,7 @@ urlpatterns = [
 
 # Add admin URLs only in development
 if settings.DEBUG:
-    urlpatterns.insert(0, path('admin/', admin.site.urls)) # Insert admin path at the beginning if in DEBUG
+    urlpatterns.insert(0, path('admin/', admin.site.urls))
 
 # Add debug toolbar URLs in development
 if settings.DEBUG:
